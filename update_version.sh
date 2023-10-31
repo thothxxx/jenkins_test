09bb284d5f440bd342470821aa965ae60773d877
longxiangxu <longxiang@elixiroftechnology.com>
2023-10-31 15:13:26 +0800

    fix file path

--- update_version.sh
+++ update_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the path to your JSON file
-json_file="/path/to/your/version.json"
+json_file="./version.json"
 
 # Read the current version from the JSON file
 current_version=$(cat "$json_file" | grep -oP '(?<=("version": )).*(?=,?)')
