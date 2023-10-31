5228645f67237375322aa0262ab2c6aa6f09366e
longxiangxu <longxiang@elixiroftechnology.com>
2023-10-31 15:11:38 +0800

    new version bash

--- update_version.sh
+++ update_version.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Increment the version number
-current_version=$(jq -r .version version.json)
-new_version="v$(echo "$current_version" | awk -F'v' '{print $2}' | awk '{printf "%02d\n", $1 + 1}')"
+# Define the path to your JSON file
+json_file="/path/to/your/version.json"
 
-# Update the JSON file with the new version
-jq --arg new_version "$new_version" '.version = $new_version' version.json > version.tmp
-mv version.tmp version.json
+# Read the current version from the JSON file
+current_version=$(cat "$json_file" | grep -oP '(?<=("version": )).*(?=,?)')
+
+# Increment the version number (e.g., by one)
+new_version=$((current_version + 1))
+
+# Update the version in the JSON file
+sed -i "s/\"version\": $current_version/\"version\": $new_version/" "$json_file"
